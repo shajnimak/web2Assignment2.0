8cf5267d8d0df3a49fe3b6e83335eb3cdd95d482
shajnimak <79740295+shajnimak@users.noreply.github.com>
2024-02-28 20:12:31 +0600

    auth fixes

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/UserController');
+const auth = require('../utils/Auth');
 
 router.post('/create', userController.createUser);
 router.post('/login', userController.loginUser); // Новый роут для аутентификации
@@ -8,5 +9,6 @@ router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+router.get('/auth', auth.authenticateUser);
 
 module.exports = router;
